96226c784bf3f8fcc2e7a75774a13d44f7697c98
jphafelin <josepablo.hafelin@gmail.com>
2023-06-25 07:49:59 +0200

    Prueba react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react'
 import ScrollToTop from './front/js/component/scrollToTop';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
 import { Home } from './front/js/pages/Home';
 import { IngresarUsuarioYContrasena } from './front/js/pages/IngresarUsuarioYContrasena';
 import { MenuPrincipal } from './front/js/pages/MenuPrincipal';
+
 import { Empresa } from './front/js/pages/Empresa';
 import { IngresarEmpresa } from './front/js/pages/IngresarEmpresa';
 import { EliminarEmpresaAPI } from './front/js/pages/EliminarEmpresaAPI';
@@ -13,6 +14,7 @@ import { ModificarEmpresaAPI } from './front/js/pages/ModificarEmpresaAPI';
 import { ModificarEmpresaId } from './front/js/pages/ModificarEmpresaId';
 import { ConsultarEmpresaAPI } from './front/js/pages/ConsultarEmpresaAPI';
 import { ConsultarEmpresaId } from './front/js/pages/ConsultarEmpresaId';
+
 import { Bots } from './front/js/pages/Bots';
 import { IngresarBot } from './front/js/pages/IngresarBot';
 import { EliminarBotAPI } from './front/js/pages/EliminarBotAPI';
@@ -20,79 +22,70 @@ import { EliminarBotId } from './front/js/pages/EliminarBotId';
 import { ModificarBotAPI } from './front/js/pages/ModificarBotAPI';
 import { ConsultarBotAPI } from './front/js/pages/ConsultarBotAPI';
 import { ConsultarBotId } from './front/js/pages/ConsultarBotId';
+
+
 import { Footer } from './front/js/component/footer';
 import { EmpresasProvider } from './front/js/store/empresasProvider';
 import { UsuariosProvider } from './front/js/store/usuariosProvider';
 
+
+
+
+
 const Layout = () => {
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+
+
+
+
   return (
     <div>
       <UsuariosProvider>
         <EmpresasProvider>
-          <Router>
-            <ScrollToTop />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/login">
-                <IngresarUsuarioYContrasena />
-              </Route>
-              <Route path="/menu">
-                <MenuPrincipal />
-              </Route>
-              <Route exact path="/empresa">
-                <Empresa />
-              </Route>
-              <Route path="/ingresar_empresa">
-                <IngresarEmpresa />
-              </Route>
-              <Route path="/eliminar_empresa">
-                <EliminarEmpresaAPI />
-              </Route>
-              <Route path="/eliminar_empresa/empresa_actual">
-                <EliminarEmpresaId />
-              </Route>
-              <Route path="/modificar_empresa">
-                <ModificarEmpresaAPI />
-              </Route>
-              <Route path="/modificar_empresa/empresa_actual">
-                <ModificarEmpresaId />
-              </Route>
-              <Route path="/consultar_empresa">
-                <ConsultarEmpresaAPI />
-              </Route>
-              <Route path="/consultar_empresa/empresa_actual">
-                <ConsultarEmpresaId />
-              </Route>
-              <Route exact path="/bots">
-                <Bots />
-              </Route>
-              <Route path="/ingresar_bot">
-                <IngresarBot />
-              </Route>
-              <Route path="/eliminar_bot">
-                <EliminarBotAPI />
-              </Route>
-              <Route path="/eliminar_bot/bot_actual">
-                <EliminarBotId />
-              </Route>
-              <Route path="/modificar_bot">
-  <ModificarBotAPI />
-</Route>
-<Route path="/consultar_bot">
-  <ConsultarBotAPI />
-</Route>
-<Route path="/consultar_bot/bot_actual">
-  <ConsultarBotId />
-</Route>
-</Switch>
-<Footer />
-</Router>
-</EmpresasProvider>
-</UsuariosProvider>
-</div>
-);
+          <BrowserRouter >
+
+            <ScrollToTop>
+              <Routes>
+
+
+                <Route element={<Home />} path="/" />
+                <Route element={<IngresarUsuarioYContrasena />} path="/login" />
+                <Route element={<MenuPrincipal />} path="/menu" />
+
+                <Route element={<Empresa />} path="/empresa" />
+                <Route element={<IngresarEmpresa />} path="/ingresar_empresa" />
+                <Route element={<EliminarEmpresaAPI />} path="/eliminar_empresa" />
+                <Route element={<EliminarEmpresaId />} path="/eliminar_empresa/empresa_actual" />
+                <Route element={<ModificarEmpresaAPI />} path="/modificar_empresa" />
+                <Route element={<ModificarEmpresaId />} path="/modificar_empresa/empresa_actual" />
+                <Route element={<ConsultarEmpresaAPI />} path="/consultar_empresa" />
+                <Route element={<ConsultarEmpresaId />} path="/consultar_empresa/empresa_actual" />
+
+                <Route element={<Bots />} path="/bots" />
+                <Route element={<IngresarBot />} path="/ingresar_bot" />
+                <Route element={<EliminarBotAPI />} path="/eliminar_bot" />
+                <Route element={<EliminarBotId />} path="/eliminar_bot/bot_actual" />
+                <Route element={<ModificarBotAPI />} path="/modificar_bot" />
+                <Route element={<ConsultarBotAPI />} path="/consultar_bot" />
+                <Route element={<ConsultarBotId />} path="/consultar_bot/bot_actual" />
+
+
+
+
+
+
+
+
+              </Routes>
+
+              <Footer />
+            </ScrollToTop>
+          </BrowserRouter>
+        </EmpresasProvider>
+      </UsuariosProvider>
+    </div>
+  );
 };
 
 export default Layout;
